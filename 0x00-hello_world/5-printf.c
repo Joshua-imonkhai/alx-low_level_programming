f2a8b8fd2ab53cc3b97702d0a12e3a8d5727fdf9
Joshua-imonkhai <joshuaimonkhia@gmail.com>
2022-09-08 14:58:16 -0700

    printf c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - the string should be printed in the put function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
